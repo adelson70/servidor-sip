f87a7e8f05136163e54bc90d5903f7f79fda997c
adelson70 <bittencourtadelson1@gmail.com>
2025-09-01 23:04:48 -0300

    traz informações necessarias para o ramal

--- handle-methods/handle-INVITE.ts
+++ handle-methods/handle-INVITE.ts
@@ -8,11 +8,37 @@ export const handleInvite = async (message: SipMessage) => {
     const ramalDestinatario = message.headers.To?.match(/sip:([^@]+)@/)?.[1];
     const tenant_id = message.headers.From?.match(/sip:[^_]+_([^@]+)@/)?.[1];
 
-    // verifica se existe esse ramal para esse tenant
-    const query = await db.query("SELECT * FROM ps_endpoints WHERE id = $1 AND tenantid = $2", [`${ramalDestinatario}_${tenant_id}`, tenant_id]);
+    const query = await db.query(`
+  SELECT 
+    e.id,
+    e.tenantid,
+    e.context,
+    e.allow,
+    e.disallow,
+    e.direct_media,
+    e.callerid,
+    e.transport,
+    e.aors,
+    e.auth,
+    c.uri,
+    c.user_agent,
+    c.expiration_time,
+    c.via_addr,
+    c.via_port,
+    c.call_id
+  FROM ps_endpoints e
+  LEFT JOIN ps_contacts c
+    ON c.endpoint = e.id
+  WHERE e.id = $1
+    AND e.tenantid = $2
+`, [`${ramalDestinatario}_${tenant_id}`, tenant_id]);
+
+
+
 
     const ramal = query.rows[0];
-    
+
+    // verifica se o ramal existe
     if (!ramal) {
         console.log("❌ Ramal não encontrado:", ramalDestinatario, "para tenant", tenant_id);
         return makeResponse({
@@ -28,7 +54,23 @@ export const handleInvite = async (message: SipMessage) => {
         });
     }
 
-    console.log("✅ Ramal encontrado:");
+    // verifica se o ramal existente esta registrado
+    if (!ramal.uri) {
+        console.log("❌ Ramal não registrado:", ramalDestinatario);
+        return makeResponse({
+            status: 480,
+            reason: "Temporarily Unavailable",
+            message: `Ramal ${ramalDestinatario} não registrado`,
+            method: message.method,
+            via: message.headers["Via"] || "",
+            from: message.headers["From"] || "",
+            to: message.headers["To"] || "",
+            callId: message.headers["Call-ID"] || "",
+            cseq: message.headers["CSeq"] || ""
+        });
+    }
+
+    console.log("✅ Ramal encontrado:", ramal);
 
     const response = 'SIP 200 OK';
 
