8775be77a39da0b025496a1a66d9c4d7ea8a1b1e
adelson70 <bittencourtadelson1@gmail.com>
2025-09-09 19:08:20 -0300

    correcao sem proxy

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ const { db } = require('./config/database');
 const { handleRegister } = require('./methods/register');
 const { handleInvite } = require('./methods/invite');
 
-require('./config/proxy').proxyConnected;
 
 const srf = new Srf();
 
